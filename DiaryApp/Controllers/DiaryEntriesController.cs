46dab3fadb20f00421b91b43b42abb59a3c05c21
RollsRoyce13 <106380187+RollsRoyce13@users.noreply.github.com>
2025-07-30 18:22:26 +0300

    Update Diary Entries Controller & View

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiaryApp.Data;
+using DiaryApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DiaryApp.Controllers
 {
     public class DiaryEntriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public DiaryEntriesController(ApplicationDbContext db) // Get ApplicationDbContext from Program.cs Services
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<DiaryEntry> objDiaryEntryList = _db.DiaryEntries.ToList(); // Get all entries from the database
+
+            return View(objDiaryEntryList); // Return the list of diary entries to the view
         }
     }
 }
